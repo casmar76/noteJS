a4464b09730f249d9e163084b1c5a6f399022443
casmar76 <153526511+casmar76@users.noreply.github.com>
2024-01-20 12:31:50 +0100

    Create smart_code.js

---
+++ smart_code.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+    (function () {
+        // Extracting URL parameters
+        var urlParams = new URLSearchParams(window.location.search);
+
+        // Function to replace spaces with '_s_' and '-' with '_d_'
+        function replaceSpacesAndDashes(inputString) {
+            return inputString.replace(/ /g, '_s_').replace(/-/g, '_d_');
+        }
+
+        // If 'tid' parameter exists, obfuscate its value and set it back
+        if (urlParams.has('tid')) {
+            var originalTid = urlParams.get('tid');
+            var obfuscatedtid = replaceSpacesAndDashes(originalTid);
+            urlParams.set('tid', obfuscatedtid);
+        }
+
+        // Get values of 'gclid', 'msclkid', or 'fbclid'
+        var adCampaignId = urlParams.get('gclid') || urlParams.get('msclkid') || urlParams.get('fbclid');
+
+        // If there are URL parameters, update links on the page
+        if (urlParams.toString()) {
+            var pageLinks = document.getElementsByTagName('a');
+
+            // Loop through all links on the page
+            for (var linkIndex = 0; linkIndex < pageLinks.length; linkIndex++) {
+                var currentLink = pageLinks[linkIndex];
+                var anchorHash = currentLink.hash;
+                var linkHref = currentLink.href.split('#')[0];
+                var linkSearchParams = new URL(linkHref, document.location.href).searchParams;
+
+                // Set 'campaignId' to 'adCampaignId' or obfuscated 'adCampaignId' if 'tid' exists
+                var modifiedCampaignId = adCampaignId;
+
+                if (linkSearchParams.has('tid') && adCampaignId) {
+                    modifiedCampaignId = replaceSpacesAndDashes(adCampaignId);
+                }
+
+                // Replace placeholders with the value of 'modifiedCampaignId' in the link
+                if (modifiedCampaignId) {
+                    linkHref = linkHref.replace('[sclid]', modifiedCampaignId).replace('%5Bsclid%5D', modifiedCampaignId);
+                }
+
+                // Append or update URL parameters in the link
+                var paramString = urlParams.toString();
+                if (linkHref.indexOf('?') === -1) {
+                    linkHref += '?' + paramString;
+                } else {
+                    linkHref += '&' + paramString;
+                }
+
+                // Update the href attribute of the link
+                currentLink.href = linkHref + anchorHash;
+            }
+        }
+    })();
+});
